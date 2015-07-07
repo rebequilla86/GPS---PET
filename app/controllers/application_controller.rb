215343855345e29abc90ee4d014d1ab28237c7c9
Rebequilla <rebequilla86@gmail.com>
2015-07-07 19:24:46 +0200

    Añadida gema deviseX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Force the user to redirect to the login page if the user was not logged in
+  before_action :authenticate_user!
 end
