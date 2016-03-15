448bf775f5eba83545e598b5ebc41bd9990050d9
Rebequilla <rebequilla86@gmail.com>
2016-03-15 20:16:36 +0100

    Añadido datepicker en la fecha de nacimiento del perro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/datepicker
+//= require jquery-ui/datepicker-es
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
