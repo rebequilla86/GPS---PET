dba211770286e85f74bc8676516875087025f778
Rebequilla <rebequilla86@gmail.com>
2016-07-19 16:00:59 +0200

    Desactivado CSRF en desarrollo y en producción

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
